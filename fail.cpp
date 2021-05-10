6f39fa3ffec8c5014712ef738ed83c6493aa4ea1
samo1182 <83954969+samo1182@users.noreply.github.com>
2021-05-10 16:51:11 +0200

    Update fail.cpp

--- fail.cpp
+++ fail.cpp
@@ -4,7 +4,10 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
 	int* arrayHeap, unsigned int arrayHeapLen) {
   int Stack[5];
 
-  Static[100] = 0; 
+  if (sizeof(Static)/sizeof(Static[0]) > 100) {
+  	Static[100] = 0; 
+  }
+  
   Stack[100] = 0; 
 
   for (int i = 0; i <= 5; i++) Stack [i] = 0;
